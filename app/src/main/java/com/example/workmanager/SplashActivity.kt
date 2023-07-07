4d06cbccb55b92f04d496746bc9172e159ea4fd6
rayanguedri <63672654+rayanguedri@users.noreply.github.com>
2023-07-07 12:45:57 +0100

    SplashScreen done

--- app/src/main/java/com/example/workmanager/SplashActivity.kt
+++ app/src/main/java/com/example/workmanager/SplashActivity.kt
@@ -1,11 +1,30 @@
 package com.example.workmanager
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.Menu
+import android.widget.ImageView
 
 class SplashActivity : AppCompatActivity() {
+    lateinit var haja:  ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        haja = findViewById(R.id.iv_note)
+
+        haja.alpha = 0f
+        haja.animate().setDuration(1500).alpha(1f).withEndAction{
+            val i = Intent(this, IntroActivity::class.java)
+            startActivity(i)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }
+
     }
+
+
 }
