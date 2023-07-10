8ece09bf7bbaa78e7d49de3ea2e50b7409e1cb6e
rayanguedri <63672654+rayanguedri@users.noreply.github.com>
2023-07-10 13:11:25 +0100

    layout improvement

---
+++ app/src/main/java/com/example/workmanager/BaseActivity.kt
@@ -0,0 +1,63 @@
+package com.example.workmanager
+
+import android.app.Dialog
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+
+open class BaseActivity : AppCompatActivity() {
+
+    private var doubleBackToExitPressedOnce = false
+
+    private lateinit var mProgressDialog: Dialog
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_base)
+    }
+
+
+fun showProgressDialog(text: String) {
+    mProgressDialog = Dialog(this)
+
+    mProgressDialog.setContentView(R.layout.dialog_progress)
+
+
+    val dialogText = mProgressDialog.findViewById<TextView>(R.id.tv_progress_message)
+
+    dialogText.text = text
+
+    mProgressDialog.setCancelable(false)
+    mProgressDialog.setCanceledOnTouchOutside(false)
+
+    mProgressDialog.show()
+    }
+
+    fun hideProgressDialog() {
+        mProgressDialog.dismiss()
+    }
+
+    fun getCurrentUserID(): String {
+        return FirebaseAuth.getInstance().currentUser!!.uid
+    }
+
+    fun doubleBackToExit() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed()
+            return
+        }
+        this.doubleBackToExitPressedOnce = true
+
+        val dialogText2 = mProgressDialog.findViewById<TextView>(R.id.please_click_back_again_to_exit)
+
+        Toast.makeText(this, resources.getString(R.string.click_again_to_exit), Toast.LENGTH_SHORT).show()
+
+
+
+    }
+
+}
--- app/src/main/java/com/example/workmanager/IntroActivity.kt
+++ app/src/main/java/com/example/workmanager/IntroActivity.kt
@@ -1,11 +1,37 @@
 package com.example.workmanager
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import android.widget.Button
+
 
 class IntroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
+
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        val signinbutton: Button? = findViewById(R.id.signInButton);
+
+        signinbutton?.setOnClickListener {
+            val intent = Intent(this, SignInActivity::class.java)
+            startActivity(intent)
+        }
+
+
+        val signupbutton: Button? = findViewById(R.id.signUpButton);
+        signupbutton?.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
     }
+
+
 }
